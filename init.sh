0241a5d64ee5f9b823f0d56105ab46c5c0a0949e
Vitalii Lozovan <vitalulozovan@gmail.com>
2020-12-11 12:49:53 +0200

    12

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
