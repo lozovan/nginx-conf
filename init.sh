79bad362d86226927541a8f1e994d3717c30a9c3
Vitalii Lozovan <vitalulozovan@gmail.com>
2020-12-14 03:06:10 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
